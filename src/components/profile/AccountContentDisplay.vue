852a6d51a177bebb26c9cf6934ed2277f89b3fa8
Lam Thanh <thanhreika2002@gmail.com>
2024-02-04 02:47:02 +0700

    Remove avatar displaying board and adjust sidebar

--- src/components/profile/AccountContentDisplay.vue
+++ src/components/profile/AccountContentDisplay.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="row">
-        <div class="col-md-3"><AccountSideBar/></div>
+        <div class="col-md-3"><AccountSidebar/></div>
         <div class="col-md-9"><AccountContent/></div>
     </div>
 </template>
 
 <script setup lang="ts">
-import AccountSideBar from '@/components/profile/account-content/AccountSideBar.vue';
+import AccountSidebar from '@/components/profile/account-content/AccountSideBar.vue';
 import AccountContent from './account-content/AccountContent.vue';
 </script>
