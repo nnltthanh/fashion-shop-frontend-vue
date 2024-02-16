98175710b045397fff870d7ae7cbd403904c19c8
Lam Thanh <thanhreika2002@gmail.com>
2024-02-16 23:20:26 +0700

    Add template of orders information

--- src/components/profile/account-content/OrderContent.vue
+++ src/components/profile/account-content/OrderContent.vue
@@ -1,37 +1,496 @@
 <template>
-    <div class="account-content mt-3">
-        <div id="info-tab" class="account-info">
-            <h2 class="account-page-title">
-                Lịch sử đơn hàng
-            </h2>
-            <div>
-                <h5>Đơn hàng của bạn</h5>
-            </div>   
-            <div class="d-grid justify-content-center">
-                <h6>Bạn chưa có đơn hàng nào...</h6>
-            </div>
+  <div class="account-content mt-3">
+    <div id="info-tab" class="account-info">
+      <h2 class="account-page-title">Lịch sử đơn hàng</h2>
+      <!-- <div class="account-page-label">
+        Đơn hàng của bạn
+        <span class="d-grid justify-content-center">
+          Bạn chưa có đơn hàng nào...
+        </span>
+      </div> -->
+      <div>
+        <div class="account-page-label">
+          Đơn hàng của bạn<span>: 2 đơn hàng</span>
         </div>
+        <div class="orders-body mt-3">
+          <div class="orders-wrapper">
+            <div href="#" class="order">
+              <div class="order-header">
+                <div>
+                  <p class="order-title">#78010670717</p>
+                  <p class="order-date">16.02.2024</p>
+                </div>
+                <div class="order-status-done">
+                  <span>Đã giao hàng</span>
+                </div>
+              </div>
+              <div class="order-body">
+                <div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/February2024/joggerutdanang4.jpg"
+                          alt="Quần Jogger Nam UT đa năng"
+                        />
+                      </a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Quần Jogger Nam UT đa năng
+                      </a>
+                      <div class="order-item-variant-label">Đen / L</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">499.000đ</div>
+                    </div>
+                  </div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/December2023/boxer_short_ke_00016_copy.jpg"
+                          alt="Pack 3 Quần Shorts Nam kẻ sọc Basics"
+                        />
+                      </a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Pack 3 Quần Shorts Nam kẻ sọc Basics
+                      </a>
+                      <div class="order-item-variant-label">Mix màu / 3XL</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">199.000đ</div>
+                    </div>
+                  </div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/April2022/DSC05753_copy1.jpg"
+                          alt="Quà tặng - Áo thun thể thao nam ProMax-S1"
+                        />
+                      </a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Quà tặng - Áo thun thể thao nam ProMax-S1
+                      </a>
+                      <div class="order-item-variant-label">Xanh Aqua / L</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">0₫</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="order-footer">
+                <div class="order-footer-left">
+                  <div>
+                    <!---->
+                    <a href="#" target="_blank" class="btn btn--outline"
+                      >Cần hỗ trợ</a
+                    >
+                  </div>
+                </div>
+                <div class="order-footer-right">
+                  <div><b>698.000đ</b></div>
+                </div>
+              </div>
+            </div>
+            <div href="#" class="order">
+              <div class="order-header">
+                <div>
+                  <p class="order-title">#78010670717</p>
+                  <p class="order-date">16.02.2024</p>
+                </div>
+                <div class="order-status-wait">
+                  <span>Chờ thanh toán...</span>
+                </div>
+              </div>
+              <div class="order-body">
+                <div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/February2024/joggerutdanang4.jpg"
+                          alt="Quần Jogger Nam UT đa năng"
+                        />
+                      </a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Quần Jogger Nam UT đa năng
+                      </a>
+                      <div class="order-item-variant-label">Đen / L</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">499.000đ</div>
+                    </div>
+                  </div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/December2023/boxer_short_ke_00016_copy.jpg"
+                          alt="Pack 3 Quần Shorts Nam kẻ sọc Basics"
+                        />
+                      </a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Pack 3 Quần Shorts Nam kẻ sọc Basics
+                      </a>
+                      <div class="order-item-variant-label">Mix màu / 3XL</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">199.000đ</div>
+                    </div>
+                  </div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/April2022/DSC05753_copy1.jpg"
+                          alt="Quà tặng - Áo thun thể thao nam ProMax-S1"
+                        />
+                      </a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Quà tặng - Áo thun thể thao nam ProMax-S1
+                      </a>
+                      <div class="order-item-variant-label">Xanh Aqua / L</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">0₫</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="order-footer">
+                <div class="order-footer-left">
+                  <div>
+                    <!---->
+                    <a href="#" target="_blank" class="btn btn--outline"
+                      >Cần hỗ trợ</a
+                    >
+                  </div>
+                </div>
+                <div class="order-footer-right">
+                  <div><b>698.000đ</b></div>
+                </div>
+              </div>
+            </div>
+            <div href="#" class="order">
+              <div class="order-header">
+                <div>
+                  <p class="order-title">#34563787096</p>
+                  <p class="order-date">16.02.2024</p>
+                </div>
+                <div class="order-status-wait">
+                  <span>Chờ thanh toán...</span>
+                </div>
+              </div>
+              <div class="order-body">
+                <div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/February2024/joggerutdanang4.jpg"
+                          alt="Quần Jogger Nam UT đa năng"
+                      /></a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Quần Jogger Nam UT đa năng
+                      </a>
+                      <div class="order-item-variant-label">Đen / L</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">499.000đ</div>
+                    </div>
+                  </div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/December2023/boxer_short_ke_00016_copy.jpg"
+                          alt="Pack 3 Quần Shorts Nam kẻ sọc Basics"
+                      /></a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Pack 3 Quần Shorts Nam kẻ sọc Basics
+                      </a>
+                      <div class="order-item-variant-label">Mix màu / 3XL</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">199.000đ</div>
+                    </div>
+                  </div>
+                  <div class="order-item">
+                    <div class="order-item-thumbnail">
+                      <a href="#" target="_blank"
+                        ><img
+                          src="https://media.coolmate.me/cdn-cgi/image/width=160,height=181,quality=80/uploads/April2022/DSC05753_copy1.jpg"
+                          alt="Quà tặng - Áo thun thể thao nam ProMax-S1"
+                      /></a>
+                    </div>
+                    <div class="order-item-info">
+                      <a href="#" target="_blank" class="order-item-title">
+                        Quà tặng - Áo thun thể thao nam ProMax-S1
+                      </a>
+                      <div class="order-item-variant-label">Xanh Aqua / L</div>
+                      <div class="order-item-quantity">x 1</div>
+                      <div class="order-item-price">0₫</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="order-footer">
+                <div class="order-footer-left">
+                  <div>
+                    <a href="#" target="_blank" class="btn btn--outline"
+                      >Cần hỗ trợ
+                    </a>
+                  </div>
+                </div>
+                <div class="order-footer-right">
+                  <div><b>698.000đ</b></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="loadingIndicator" class="loading-indicator">
+          <div class="loader"></div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 .account-content {
-    position: relative;
-    /* display: flex; */
-    align-items: center;
-    padding: 3rem 3rem;
-    transition: all .2s;
-    border-radius: 0.5rem;
-    font-size: 1rem;
-    box-sizing: border-box;
-    background-color: white;
-    width: 100%;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  position: relative;
+  /* display: flex; */
+  align-items: center;
+  padding: 3rem 3rem;
+  transition: all 0.2s;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  box-sizing: border-box;
+  background-color: white;
+  width: 100%;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 }
 
 .account-page-title {
-    margin-bottom: 25px;
-    font-size: 2.5rem;
+  margin-bottom: 25px;
+  font-size: 2.5rem;
 }
 
+.account-page-label {
+  font-size: 18px;
+  color: rgba(0, 0, 0, 0.5333333333);
+}
+
+a {
+  background-color: transparent;
+  text-decoration: none;
+  color: inherit;
+  transition: all 0.2s;
+}
+
+.order:not(:last-child) {
+  margin-bottom: 27px;
+}
+
+.order {
+  position: relative;
+  display: block;
+}
+
+.order-header {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  background-color: #2f5acf;
+  border-radius: 0.5rem;
+  padding: 10px 30px;
+  position: relative;
+  z-index: 2;
+  box-sizing: border-box;
+}
+
+.order-header .order-title {
+  font-weight: bold;
+  font-size: 1.1rem;
+}
+
+.order-title,
+.order-date {
+  margin: 0px;
+}
+
+.order-header .order-date {
+  font-size: 0.8rem;
+}
+
+.order-status-wait {
+  display: flex;
+  padding: 6px 12px;
+  justify-content: center;
+  align-items: center;
+  background-color: #f2fd5d;
+  border-radius: 47px;
+  color: #000;
+}
+
+.order-status-done {
+  display: flex;
+  padding: 6px 12px;
+  justify-content: center;
+  align-items: center;
+  background-color: #57e37f;
+  border-radius: 47px;
+  color: #000;
+}
+
+.order-body {
+  border: 1px solid #d9d9d9;
+  background-color: rgb(232, 232, 232);
+  margin-top: -16px;
+  position: relative;
+  z-index: 1;
+  padding-top: 16px;
+}
+
+.order-item:not(:last-child) {
+  border-bottom: 1px solid #d9d9d9;
+}
+
+.order-item {
+  padding: 1rem 30px;
+  display: flex;
+  width: 100%;
+  position: relative;
+}
+
+.order-item-thumbnail {
+  position: relative;
+  width: 100px;
+  margin-right: 1rem;
+}
+
+.order-item-thumbnail::before {
+  content: "";
+  display: block;
+  padding-top: 111.7647058824%;
+  height: 0;
+  width: 100%;
+}
+
+.order-item-thumbnail img {
+  border-radius: 8px;
+}
+
+.order-item-thumbnail img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+}
+
+.order-item-info {
+  flex: 1;
+}
+
+.order-item-title,
+.order-item-price {
+  font-weight: 600;
+}
+
+.order-item-variant-label,
+.order-item-quantity {
+  color: rgba(0, 0, 0, 0.6);
+}
+
+.order-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  border: 1px solid #d9d9d9;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 0.5rem 30px 0.5rem;
+  border-radius: 0 0 0.5rem 0.5rem;
+  position: relative;
+  z-index: 1;
+  box-sizing: border-box;
+}
+
+.order-footer-left {
+  display: flex;
+}
+
+/* .order-footer-left .btn:last-of-type {
+  margin-right: 0;
+} */
+
+.order-footer-left .btn {
+  height: auto;
+  padding: 0.5rem 2rem;
+  border-radius: 100vmax;
+  line-height: 1.5;
+  box-sizing: border-box;
+  font-family: "CriteriaCF", "Pangea", sans-serif;
+  margin-right: 10px;
+  border: 2px solid #000;
+  cursor: pointer;
+}
+
+.btn--outline {
+  background-color: transparent;
+  color: #000;
+}
+
+.btn--outline:hover {
+  background-color: #000;
+  color: #fff;
+  border: 2px solid #000;
+}
+
+.order-footer-right {
+  text-align: right;
+}
+
+.loading-indicator {
+  display: none;
+  position: fixed;
+  z-index: 9999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  justify-content: center;
+  align-items: center;
+}
+
+.loader {
+  border: 5px solid #f3f3f3;
+  border-top: 5px solid #000;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin-d810b37c 2s linear infinite;
+}
+
+*,
+:after,
+:before {
+  box-sizing: border-box;
+}
 </style>
