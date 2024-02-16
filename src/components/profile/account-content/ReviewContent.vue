7fab7a272ea17b7af216f6b9b84bc955e6ebe76f
Lam Thanh <thanhreika2002@gmail.com>
2024-02-16 23:18:38 +0700

    Add reviews information component for profile page

---
+++ src/components/profile/account-content/ReviewContent.vue
@@ -0,0 +1,300 @@
+<template>
+  <div class="account-content mt-3">
+    <div id="info-tab" class="account-info">
+      <h2 class="account-page-title">Đánh giá và phản hồi</h2>
+      <!-- <div class="account-page-label mt-4">Bạn chưa có đánh giá nào...</div> -->
+      <div>
+        <div class="grid-column mt-3">
+          <div class="reviews-listing-items">
+            <div class="reviews-listing-item">
+              <div class="reviews-listing-content">
+                <div class="reviews-rating">
+                  <div class="reviews-rating-star is-full"></div>
+                  <div class="reviews-rating-star is-full"></div>
+                  <div class="reviews-rating-star is-full"></div>
+                  <div class="reviews-rating-star is-full"></div>
+                  <div class="reviews-rating-star is-full"></div>
+                </div>
+                <div class="reviews-order">
+                  <div class="order-item-title">Áo thun thể thao nam ProMax-S1</div>
+                  <div class="order-item-variant-label">Xanh Aqua / L</div>
+                </div>
+                <div class="reviews-listing-description">
+                  <p>
+                    Okeee
+                  </p>
+                  <!-- <div
+                    class="reviews-listing-gallery"
+                    rel-script="product-gallery-popup"
+                  >
+                    <a
+                      href="#"
+                      class="reviews-listing-image"
+                      rel-script="product-lightbox-gallery"
+                      data-index="0"
+                      data-image="https://i.pinimg.com/564x/0c/2a/0b/0c2a0bb82f4565303156c201d3276064.jpg"
+                    >
+                      <img
+                        src="https://i.pinimg.com/564x/0c/2a/0b/0c2a0bb82f4565303156c201d3276064.jpg"
+                        alt="0"
+                      />
+                    </a>
+                  </div> -->
+                  <p class="reviews-listing-feedback">
+                    Phản hồi
+                  </p>
+                  <span class="reviews-listing-date">16.02.2024 23:10</span>
+                </div>
+              </div>
+            </div>
+            <div class="reviews-listing-item">
+              <div class="reviews-listing-content">
+                <div class="reviews-rating">
+                  <div class="reviews-rating-star is-full"></div>
+                  <div class="reviews-rating-star is-full"></div>
+                  <div class="reviews-rating-star is-full"></div>
+                  <div class="reviews-rating-star is-full"></div>
+                  <div class="reviews-rating-star is-full"></div>
+                </div>
+                <div class="reviews-order">
+                  <div class="order-item-title">Quần Jogger Nam UT đa năng</div>
+                  <div class="order-item-variant-label">Trắng / M</div>
+                </div>
+                <div class="reviews-listing-description">
+                  <p>
+                    Okeee
+                  </p>
+                  <div
+                    class="reviews-listing-gallery"
+                    rel-script="product-gallery-popup"
+                  >
+                    <a
+                      href="#"
+                      class="reviews-listing-image"
+                      rel-script="product-lightbox-gallery"
+                      data-index="0"
+                      data-image="https://i.pinimg.com/564x/0c/2a/0b/0c2a0bb82f4565303156c201d3276064.jpg"
+                    >
+                      <img
+                        src="https://i.pinimg.com/564x/0c/2a/0b/0c2a0bb82f4565303156c201d3276064.jpg"
+                        alt="0"
+                      />
+                    </a>
+                  </div>
+                  <p class="reviews-listing-feedback">
+                    Phản hồi
+                  </p>
+                  <span class="reviews-listing-date">16.02.2024 22:00</span>
+                </div>
+              </div>
+            </div>
+            <div class="reviews-listing-item">
+              <div class="reviews-listing-content">
+                <div class="reviews-rating">
+                  <div class="reviews-rating-star is-active"></div>
+                  <div class="reviews-rating-star is-active"></div>
+                  <div class="reviews-rating-star is-half"></div>
+                  <div class="reviews-rating-star"></div>
+                  <div class="reviews-rating-star"></div>
+                </div>
+                <div class="reviews-order">
+                  <div class="order-item-title">Pack 3 Quần Shorts Nam kẻ sọc Basics</div>
+                  <div class="order-item-variant-label">Mix màu / 3XL</div>
+                </div>
+                <div class="reviews-listing-description">
+                  <p>
+                    Sản phẩm okela nè
+                  </p>
+                  <div
+                    class="reviews-listing-gallery"
+                    rel-script="product-gallery-popup"
+                  >
+                    <a
+                      href="#"
+                      class="reviews-listing-image"
+                      rel-script="product-lightbox-gallery"
+                      data-index="0"
+                      data-image="https://i.pinimg.com/564x/82/74/c4/8274c45adcf0b0781ab95f9f6692bb2d.jpg"
+                    >
+                      <img
+                        src="https://i.pinimg.com/564x/82/74/c4/8274c45adcf0b0781ab95f9f6692bb2d.jpg"
+                        alt="0"
+                      />
+                    </a>
+                    <a
+                      href="#"
+                      class="reviews-listing-image"
+                      rel-script="product-lightbox-gallery"
+                      data-index="0"
+                      data-image="https://i.pinimg.com/564x/82/74/c4/8274c45adcf0b0781ab95f9f6692bb2d.jpg"
+                    >
+                      <img
+                        src="https://i.pinimg.com/564x/82/74/c4/8274c45adcf0b0781ab95f9f6692bb2d.jpg"
+                        alt="0"
+                      />
+                    </a>
+                  </div>
+                  <p class="reviews-listing-feedback">
+                    Phản hồi
+                  </p>
+                  <span class="reviews-listing-date"> 16.02.2024 19:14</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.account-content {
+  position: relative;
+  /* display: flex; */
+  align-items: center;
+  padding: 3rem 3rem;
+  transition: all 0.2s;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  box-sizing: border-box;
+  background-color: white;
+  width: 100%;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+.account-page-label {
+  font-size: 18px;
+  color: rgba(0, 0, 0, 0.5333333333);
+}
+
+.grid-column {
+  position: relative;
+  box-sizing: border-box;
+  min-height: 1px;
+  vertical-align: top;
+  margin-left: 0 !important;
+  margin-right: 0 !important;
+  padding: 9px;
+  width: 100%;
+  
+}
+
+.reviews-listing-item {
+  display: flex;
+  padding: 15px 0;
+  border-bottom: 1px solid #d9d9d9;
+}
+
+.reviews-listing-item:is(:first-child) {
+  border-top: 1px solid #d9d9d9;    
+}
+
+.reviews-listing-content {
+  flex: 1;
+}
+
+.reviews-rating {
+  display: flex;
+  align-items: center;
+  margin-left: -3px;
+  margin-right: -3px;
+}
+
+.reviews-rating-star.is-active,
+.reviews-rating-star.is-full {
+  background-image: url(https://www.coolmate.me/images/star.svg?2a5188496861d26e5547c524320ec875);
+}
+
+.reviews-rating-star {
+  display: block;
+  width: 20px;
+  height: 20px;
+  margin: 0 3px 1px;
+  background-image: url(https://www.coolmate.me/images/star-new.svg?08a379c24952a980d5430515abb8be4e);
+  background-repeat: no-repeat;
+  background-position: 50%;
+  background-size: contain;
+}
+
+.reviews-rating-star.is-half {
+  background-image: url(https://www.coolmate.me/images/star-half.svg?8aea9e9938db110e66ea06732737184a);
+}
+
+.reviews-order {
+  font-size: 14px;
+  line-height: 18px;
+  color: #000;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  line-height: 1.5em;
+}
+
+.order-item-variant-label {
+  font-weight: 400;
+  color: rgba(0, 0, 0, 0.4);
+  font-style: italic;
+  font-size: 12px;
+}
+
+.reviews-listing-description {
+  font-size: 14px;
+  line-height: 18px;
+  letter-spacing: 0.03em;
+  color: #000;
+}
+
+.reviews-listing-description p {
+  margin: 0;
+  font-weight: 400;
+}
+
+.reviews-listing-feedback {
+  padding: 14px 18px;
+  border-radius: 16px;
+  margin-top: 15px !important;
+  line-height: 1.5em;
+  border: 1px solid #d9d9d9;
+  background-color: #d9d9d9;
+  width: 100%;
+}
+
+.reviews-listing-date {
+  display: block;
+  margin-top: 20px;
+  opacity: 0.5;
+}
+
+.reviews-listing-gallery {
+  display: flex;
+  margin-left: -4px;
+  margin-right: -4px;
+  margin-top: 10px;
+}
+
+.reviews-listing-image {
+  overflow: hidden;
+  border-radius: 8px;
+  width: 60px;
+  margin: 0 4px;
+  position: relative;
+}
+
+.reviews-listing-image:before {
+  content: "";
+  display: block;
+  padding-top: 100%;
+  height: 0;
+  width: 100%;
+}
+
+.reviews-listing-image img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+}
+</style>
