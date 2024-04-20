f37a6bca903749b96ab9eeee1cec050f76afba6d
lytantai <lytantai02@gmail.com>
2024-04-20 13:16:09 +0700

    create product form

--- src/components/manager/ProductManagement.vue
+++ src/components/manager/ProductManagement.vue
@@ -1,7 +1,8 @@
 <template>
+    <ProductForm />
     <div class="customer-management m-3">
         <div class="w-full">
-            <div class="w-full">
+            <div class="w-full">    
                 <!-- Các thông báo -->
                 <div class="alerts">
                     <div v-if="isUpdatedOK"
@@ -26,45 +27,26 @@
                         <label class="text-gray-700" for="id">
                             ID:
                         </label>
-                        <input v-model="selectedEmployee.id"
+                        <input v-model="selectedProductDetail.id"
                             class="input-id w-full bg-gray-200 text-gray-800 py-2 px-3 rounded-md focus:outline-none">
                     </div>
-                    <div class="w-1/6 mr-2">
+                    <div class="mr-2">
                         <label class="text-gray-700" for="id">
-                            Tên tài khoản:
+                            Tên sản phẩm:
                         </label>
-                        <input v-model="selectedEmployee.account"
+                        <input v-model="selectedProductDetail.product.name"
                             class="w-full bg-gray-200 text-gray-800 py-2 px-3 rounded-md focus:outline-none">
-                    </div>
-                    <div class="w-1/6 mr-2">
-                        <label class="text-gray-700" for="id">
-                            Trạng thái
-                        </label>
-                        <div
-                            class="input-container w-full bg-gray-200 text-gray-800 py-2 px-3 rounded-md focus:outline-none focus:bg-white">
-                            {{ selectedEmployee?.account == '' ? 'Chọn tk...' :
-                                (selectedEmployee?.locked ? "Đã bị khoá" : "Hoạt động") }}</div>
                     </div>
                     <div class="w-2/6 mr-2 flex items-end">
-                        <button @click="blockAndUnblock(selectedEmployee)"
-                            class=" mr-2 bg-gradient-to-b from-green-500 to-sky-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            {{ selectedEmployee?.locked ? "Mở khoá" : "Khoá" }}
-                        </button>
-                        <button @click="searchEmployee"
+                        <button @click="searchProductDetail"
                             class="mr-2 bg-gradient-to-b from-blue-500 to-sky-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tìm
-                            kiếm</button>
+                            kiếm
+                        </button>
+                        <button @click="searchProductDetail"
+                            class="mr-2 bg-gradient-to-b from-green-500 to-sky-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Thêm sản phẩm
+                        </button>
                     </div>
-                    <!-- <div class="filter-employee w-1/6 mr-2">
-                        <label for="userType" class="text-gray-700">Chức vụ:</label>
-                        <select v-model="selectedUserType" @change="filterEmployeesByUserType" id="userType"
-                            class="w-full bg-gray-200 text-gray-800 py-2 px-3 rounded-md focus:outline-none">
-                            <option value="all">Tất cả</option>
-                            <option value="admin">Admin</option>
-                            <option value="manager">Manager</option>
-                            <option value="senior_manager">Senior Manager</option>
-                            <option value="staff">Staff</option>
-                        </select>
-                    </div> -->
                 </div>
                 <div class="flex items-end mt-2">Tổng số: {{ currentTotalProductDetail }}</div>
             </div>
@@ -77,19 +59,19 @@
                         <th scope="col" class="px-4 py-3">
                             ID
                         </th>
-                        <th scope="col" class="px-4 py-3">
+                        <th scope="col" class="px-4 py-3 w-1/10">
                             ID Sản phẩm
                         </th>
-                        <th scope="col" class="px-4 py-3">
+                        <th scope="col" class="px-4 py-3 w-1/6">
                             Tên sản phẩm
                         </th>
                         <th scope="col" class="px-4 py-3">
-                            Giá bán
+                            Giá
                         </th>
-                        <th scope="col" class="px-4 py-3">
+                        <!-- <th scope="col" class="px-4 py-3 w-1/12">
                             Giá gốc
-                        </th>
-                        <th scope="col" class="px-4 py-3">
+                        </th> -->
+                        <th scope="col" class="px-4 py-3 w-1/10">
                             Giảm giá (%)
                         </th>
                         <th scope="col" class="px-4 py-3">
@@ -99,10 +81,10 @@
                             Chất liệu
                         </th>
                         <th scope="col" class="px-4 py-3">
-                            Kích cỡ
+                            Màu
                         </th>
                         <th scope="col" class="px-4 py-3">
-                            Màu
+                            Kích cỡ
                         </th>
                         <th scope="col" class="px-4 py-3">
                             Hiện có
@@ -110,51 +92,67 @@
                         <th scope="col" class="7px-4 py-3">
                             Đã bán
                         </th>
-                        <th scope="col" class="px-4 py-3">
+                        <th scope="col" class="px-4 py-3 text-center">
                             Ảnh
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(item, index) in productDetails" :key="index" @click="selectEmployee(employee)"
+                    <tr v-for="(item, index) in productDetails" :key="index" @click="selectProductDetail(item)"
                         class="row-data border-b dark:bg-gray-800 cursor-pointer">
                         <th scope="row" class="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ item.id ? item.id :
                                 "Chưa cập nhật" }}
                         </th>
-                        <td class="px-4 py-4">
+                        <td class="px-4 py-4 w-1/10">
                             {{ item.product.id ? item.product.id :
                                 "Chưa cập nhật" }}
                         </td>
                         <!-- <td class="px-6 py-4 overflow-x-auto custom-scrollbar-cell" style="max-width: 150px"> -->
-                        <td class="px-4 py-4">
+                        <td class="px-4 py-4 w-1/6">
                             {{ item.product.name ? item.product.name :
                                 "Chưa cập nhật" }}
                         </td>
                         <td class="px-4 py-4">
-                            {{ item.locked ? "Đã khoá" :
-                                "Hoạt động" }}
-                        </td>
-                        <td class="px-4 py-4">
-                            {{ item.phone ? item.phone :
+                            {{ item.product.price ? item.product.price :
                                 "Chưa cập nhật" }}
                         </td>
-                        <td class="px-4 py-4">
-                            {{ item.email ? item.email :
+                        <!-- <td class="px-4 py-4 w-1/12">
+                            {{ item.product.cost ? item.product.cost :
                                 "Chưa cập nhật" }}
-                        </td>
+                        </td> -->
                         <!-- <td class="px-6 py-4 overflow-x-auto custom-scrollbar-cell" style="max-width: 150px"> -->
-                        <td class="px-4 py-4">
-                            {{ item.dob ? item.dob :
-                                "Chưa cập nhật" }}
+                        <td class="px-4 py-4 w-1/10">
+                            {{ item.product.salePercent ? item.product.salePercent :
+                                0 }}
                         </td>
                         <td class="px-4 py-4 overflow-x-auto custom-scrollbar-cell" style="max-width: 150px">
-                            {{ item.address ? item.address :
+                            {{ item.product.type ? item.product.type :
                                 "Chưa cập nhật" }}
                         </td>
                         <td class="px-4 py-4">
-                            {{ item.userType ? item.userType :
+                            {{ item.product.material ? item.product.material :
                                 "Chưa cập nhật" }}
+                        </td>
+                        <td class="px-4 py-4">
+                            {{ item.color ? item.color :
+                                "Chưa cập nhật" }}
+                        </td>
+                        <td class="px-4 py-4">
+                            {{ item.size ? item.size :
+                                "Chưa cập nhật" }}
+                        </td>
+                        <td class="px-4 py-4">
+                            {{ item.quantity ? item.quantity :
+                                "Chưa cập nhật" }}
+                        </td>
+                        <td class="px-4 py-4">
+                            {{ item.sold ? item.sold :
+                                "Chưa cập nhật" }}
+                        </td>
+                        <td class="px-4 py-4">
+                            <img :src="item.imageLinks[0] ? item.imageLinks[0] :
+                                'Chưa cập nhật'" alt="">
                         </td>
                     </tr>
                 </tbody>
@@ -164,55 +162,54 @@
 </template>
 
 <script setup lang="ts">
+import ProductForm from '@/components/manager/ProductForm.vue';
 import { ref, onBeforeMount } from 'vue';
 import axios from 'axios';
-
-export type ProductObject = {
-  id: number,
-  name: String,
-  price: number,
-  cost: number,
-  salePercent: number,
-  type: String,
-  material: String,
-  imageData: {
+interface ProductObject {
     id: number,
-    base64String: String,
-    type: String
-  },
-  createdAt: Date,
-  updatedAt: Date,
-  sold: number,
+    name: String,
+    price: number,
+    cost: number,
+    salePercent: number,
+    type: String,
+    material: String,
+    imageData: {
+        id: number,
+        base64String: String,
+        type: String
+    },
+    createdAt: Date,
+    updatedAt: Date,
+    sold: number,
 }
-
-export type ProductDetailObject = {
-  id: number,
-  size: String,
-  color: String,
-  unit: String,
-  quantity: number,
-  product: ProductObject,
-  imageLinks: string
+interface ProductDetailObject {
+    id: number,
+    size: String,
+    color: String,
+    unit: String,
+    quantity: number,
+    product: ProductObject,
+    imageLinks: string
 }
 
 const productDetails = ref<ProductDetailObject[] | null>(null);
-// const selectedProductDetail = ref<ProductDetailObject>({
-//     id: '',
-//     productId: '',
-//     name: '',
-//     price: '',
-//     cost: '',
-//     salePercent: '',
-//     type: '',
-//     material: '',
-//     size: '',
-//     color: '',
-//     unit: '',
-//     quantity: '',
-//     sold: '',
-//     product: '',
-//     imageLinks: ''
-// });
+const selectedProductDetail = ref<ProductDetailObject>({
+    id: '',
+    productId: '',
+    name: '',
+    price: '',
+    cost: '',
+    salePercent: '',
+    type: '',
+    material: '',
+    size: '',
+    color: '',
+    unit: '',
+    quantity: '',
+    sold: '',
+    product: '',
+    imageLinks: ''
+});
 
 const currentTotalProductDetail = ref<number>(0);
 onBeforeMount(async () => {
@@ -220,105 +217,120 @@ onBeforeMount(async () => {
         const response = await axios.get(`http://localhost:8080/products/details`);
         productDetails.value = response.data;
         currentTotalProductDetail.value = productDetails.value?.length!;
-        console.log(productDetails.value);
+        productDetails.value.forEach(detail => {
+            const imageLinksArray = detail.imageLinks.split(", ");
+            detail.imageLinks = imageLinksArray;
+        })
     } catch (error) {
         console.error('Lỗi khi lấy thông tin sản phẩm', error);
     }
 });
 const isNotEnteredID = ref(false);
 const isUpdatedOK = ref(false);
-// const blockAndUnblock = async (employee: Employee) => {
-//     try {
-//         let idToSearch = parseInt(selectedEmployee.value.id);
-//         console.log(idToSearch);
-//         if (isNaN(idToSearch)) {
-//             isNotEnteredID.value = true;
-//             setTimeout(() => {
-//                 isNotEnteredID.value = false;
-//             }, 1500);
-//             return;
-//         }
-//         const response = await axios.put(`http://localhost:8080/users/${employee.id}/updateLockedStatus`);
-//         if (response.status === 200) {
-//             let index = employees.value?.findIndex((a) => a.id === employee.id)
-//             employee.locked = !employee.locked;
-//             if (index != undefined && index >= 0) {
-//                 employees.value?.splice(index, 1, employee)
-//                 isUpdatedOK.value = true;
-//                 setTimeout(() => {
-//                     isUpdatedOK.value = false;
-//                 }, 1500);
-//             } else {
-//                 return;
-//             }
-//         } else {
-//             console.error('Error updating lock status');
-//         }
-//     } catch (error) {
-//         console.error('Error updating lock status:', error);
-//     }
-// };
-
-// const isNotFoundEmployee = ref(false);
-// const searchEmployee = () => {
-//     let idToSearch = parseInt(selectedEmployee.value.id);
-//     console.log(idToSearch);
-//     if (isNaN(idToSearch)) {
-//         isNotEnteredID.value = true;
-//         setTimeout(() => {
-//             isNotEnteredID.value = false;
-//         }, 1500);
-//     } else {
-//         const index = employees.value?.findIndex(employee => parseInt(employee.id) === idToSearch);
-//         if (index !== undefined && index !== -1) {
-//             const element = document.querySelectorAll('.row-data')[index];
-//             element.scrollIntoView({ behavior: 'smooth', block: 'center' });
-//             element.classList.add('found');
-//             setTimeout(() => {
-//                 element.classList.remove('found');
-//             }, 4000);
-//         } else {
-//             isNotFoundEmployee.value = true;
-//             setTimeout(() => {
-//                 isNotFoundEmployee.value = false;
-//             }, 1500);
-//         }
-//     }
-// };
-
-const selectEmployee = (employee: Employee) => {
-    selectedEmployee.value = { ...employee };
-};
-
-const selectedUserType = ref<string>('all');
-
-const filterEmployeesByUserType = () => {
-    currentTotalAccount.value = employees.value?.length!;
-    const table = document.getElementById("table-data");
-    const tr = table?.getElementsByTagName("tr");
-    let tdContent;
-    let temp = 0;
-    if (tr !== undefined) {
-        for (let i = 0; i < tr?.length; i++) {
-            const td = tr[i].getElementsByTagName("td")[2];
-            if (td) {
-                tdContent = td.textContent?.trim();
-                // console.log(tdContent);
-                if (tdContent !== selectedUserType.value && selectedUserType.value !== 'all' && tr != undefined) {
-                    // --currentTotalAccount;
-                    tr[i].style.display = "none";
-                    temp++;
-                } else {
-                    tr[i].style.display = "";
-                }
-            }
+const blockAndUnblock = async (employee: Employee) => {
+    try {
+        let idToSearch = parseInt(selectedEmployee.value.id);
+        console.log(idToSearch);
+        if (isNaN(idToSearch)) {
+            isNotEnteredID.value = true;
+            setTimeout(() => {
+                isNotEnteredID.value = false;
+            }, 1500);
+            return;
         }
-        currentTotalAccount.value = employees.value?.length! - temp;
-        console.log(currentTotalAccount.value);
+        const response = await axios.put(`http://localhost:8080/users/${employee.id}/updateLockedStatus`);
+        if (response.status === 200) {
+            let index = employees.value?.findIndex((a) => a.id === employee.id)
+            employee.locked = !employee.locked;
+            if (index != undefined && index >= 0) {
+                employees.value?.splice(index, 1, employee)
+                isUpdatedOK.value = true;
+                setTimeout(() => {
+                    isUpdatedOK.value = false;
+                }, 1500);
+            } else {
+                return;
+            }
+        } else {
+            console.error('Error updating lock status');
+        }
+    } catch (error) {
+        console.error('Error updating lock status:', error);
     }
 };
+
+const isNotFoundProductDetail = ref(false);
+const searchProductDetail = () => {
+    let idToSearch = parseInt(selectedProductDetail.value.id);
+    console.log(idToSearch);
+    if (isNaN(idToSearch)) {
+        isNotEnteredID.value = true;
+        setTimeout(() => {
+            isNotEnteredID.value = false;
+        }, 1500);
+    } else {
+        const index = productDetails.value?.findIndex(detail => parseInt(detail.id) === idToSearch);
+        if (index !== undefined && index !== -1) {
+            const element = document.querySelectorAll('.row-data')[index];
+            element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            element.classList.add('found');
+            setTimeout(() => {
+                element.classList.remove('found');
+            }, 4000);
+        } else {
+            isNotFoundProductDetail.value = true;
+            setTimeout(() => {
+                isNotFoundProductDetail.value = false;
+            }, 1500);
+        }
+    }
+};
+
+const selectProductDetail = (detail: ProductDetailObject) => {
+    selectedProductDetail.value = { ...detail };
+};
+
+// const selectedUserType = ref<string>('all');
+
+// const filterEmployeesByUserType = () => {
+//     currentTotalAccount.value = employees.value?.length!;
+//     const table = document.getElementById("table-data");
+//     const tr = table?.getElementsByTagName("tr");
+//     let tdContent;
+//     let temp = 0;
+//     if (tr !== undefined) {
+//         for (let i = 0; i < tr?.length; i++) {
+//             const td = tr[i].getElementsByTagName("td")[2];
+//             if (td) {
+//                 tdContent = td.textContent?.trim();
+//                 // console.log(tdContent);
+//                 if (tdContent !== selectedUserType.value && selectedUserType.value !== 'all' && tr != undefined) {
+//                     // --currentTotalAccount;
+//                     tr[i].style.display = "none";
+//                     temp++;
+//                 } else {
+//                     tr[i].style.display = "";
+//                 }
+//             }
+//         }
+//         currentTotalAccount.value = employees.value?.length! - temp;
+//         console.log(currentTotalAccount.value);
+//     }
+// };
 </script>
 <style scoped>
+.w-1\/8 {
+    width: 12.5%;
+}
+
+.w-1\/10 {
+    width: 10%;
+}
+
+.w-1\/12 {
+    width: 8.33%;
+}
+
 .fixed-header {
     position: sticky;
     top: 0;
